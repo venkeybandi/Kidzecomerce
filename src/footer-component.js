06409ccf58ae91164603a01dacbd683ef027b3ed
venkeybandi <70914716+venkeybandi@users.noreply.github.com>
2021-01-02 14:33:38 +0530

    Update footer-component.js

--- src/footer-component.js
+++ src/footer-component.js
@@ -12,9 +12,6 @@ class FooterComponent extends PolymerElement {
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
-    <!-- Compiled and minified JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    
     <style include="shared-styles">
         footer{
             padding-top: 50px;
