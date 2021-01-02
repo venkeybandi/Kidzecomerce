9b0d33e211fc83b82e0258bc36abf4879a2310bc
venkeybandi <70914716+venkeybandi@users.noreply.github.com>
2021-01-02 14:52:17 +0530

    Update shared-products-style.js

--- src/shared-products-style.js
+++ src/shared-products-style.js
@@ -1,12 +1,3 @@
-/**
- * @license
- * Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
- * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
- * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
- * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
- * Code distributed by Google as part of the polymer project is also
- * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
- */
 
 import '@polymer/polymer/polymer-element.js';
 
@@ -31,6 +22,12 @@ $_documentContainer.innerHTML = `<dom-module id="shared-products-style">
       padding-top: 50px;
       padding-bottom: 50px;
     }
+    .products-container{
+      display: flex;
+      justify-content: center;
+      flex-wrap: wrap;
+      margin-top: 50px;
+  }
     .product-container{
       display: flex;
       justify-content: space-between;
