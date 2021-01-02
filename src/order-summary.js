44782d98f01bc1c8bb4e01e7e1073abf4944c739
venkeybandi <70914716+venkeybandi@users.noreply.github.com>
2021-01-02 14:45:08 +0530

    Update order-summary.js

--- src/order-summary.js
+++ src/order-summary.js
@@ -1,48 +1,66 @@
+// Import the Polymer library and html helper function
+// Import the cart-summary-styles
 
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
-import './shared-styles.js';
-import './newarival-products.js';
 import './cart-summary-styles.js';
 
+// Define the new element as a class
 class OrderSummary extends PolymerElement {
+// Provide a DOM template for the element
   static get template() {
-    return html`    
-    <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-
-    <!-- Compiled and minified JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    
-      <style include="cart-summary-styles">
+    return html`      
+        <style include="cart-summary-styles">
    
-      </style>
+        </style>
       
-    <div class="order-summary">
-        <div class="orderSummary">
-            <table style="width:100%">
+        <div class="order-summary">
+            <div class="orderSummary">
                 <h1>Order Summary</h1>
-                <tr>
-                    <td><h4 class="ordertitle">Orders amount:</h4</td>
-                    <td><h4 class="orderlvalue"><i class="fa fa-rupee"></i><span>{{item.price}}</span></h4></td>
-                </tr>
-                <tr>
-                    <td><h4 class="taxtitle">Tax amount:</h4></td>
-                    <td><h4 class="taxamount"><i class="fa fa-rupee"></i>{{item.2}} + 200</h4></td>
-                </tr>
-                <tr>
-                    <td><h4 class="taxtitle">Delivary charges:</h4></td>
-                    <td><h4 class="taxamount"><i class="fa fa-rupee"></i> 100</h4></td>
-                </tr>
-                <tr>
-                    <td><h3 class="taxtitle">Total amount</h3></td>
-                    <td><h3 class="taxamount"><i class="fa fa-rupee"></i> 3300</h3></td>
-                </tr>
-            </table>
-            
-        </div>
-    </div> 
+
+                <div class="producttotal line1">
+                    <span class="ordertitle">Orders amount:</span>
+                    <span class="orderlvalue"><iron-icon class="small" src = "./src/images/currency-inr.svg"></iron-icon>
+                        [[cartCost]]
+                    </span>
+                </div>
+
+                <div class="producttotal line2">
+                    <span class="ordertitle">delivery charges:</span>
+                    <span class="orderlvalue"><iron-icon class="small" src = "./src/images/currency-inr.svg"></iron-icon>
+                        [[dellivarycharges]]
+                    </span>
+                </div>
+
+                <div class="producttotal line3">
+                    <span class="ordertitle">Tax:</span>
+                    <span class="orderlvalue"><iron-icon class="small" src = "./src/images/currency-inr.svg"></iron-icon>
+                        [[taxamount]]
+                    </span>
+                </div>
+
+                <div class="producttotal line4">
+                    <span class="ordertitle">Total amount</span>
+                    <span class="orderlvalue"><iron-icon class="small" src = "./src/images/currency-inr.svg"></iron-icon>
+                        [[summarytotalamount]]
+                    </span>
+                </div>
+            </div>
+        </div> 
     `;
   }
-}
+    ready() {
+        super.ready();
+        // this.itemList = JSON.parse(window.localStorage.getItem('productsInLocalStore'));
 
+        //summary calculations section Amount, Delivary charges, 
+        this.cartCost = parseInt(window.localStorage.getItem('totalamount'));
+        //Delivary charges
+        this.dellivarycharges = this.cartCost * 0.02;
+        //TAX charges
+        this.taxamount = this.cartCost * 0.01;
+        // TOTAL Order summary amount
+        this.summarytotalamount = Math.round(this.cartCost + this.dellivarycharges + this.taxamount);
+    }
+}
+//registering into the webbrowser using cusomelement
 window.customElements.define('order-summary', OrderSummary);
