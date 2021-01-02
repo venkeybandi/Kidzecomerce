1b8b87b8273c4e309d579743301b9e4c025f31db
venkeybandi <70914716+venkeybandi@users.noreply.github.com>
2021-01-02 14:32:58 +0530

    Update footer-component.js

--- src/footer-component.js
+++ src/footer-component.js
@@ -1,9 +1,13 @@
-
+// Import the Polymer library and html helper function
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 import './shared-styles.js';
 
+// Define the new element as a class
 class FooterComponent extends PolymerElement {
+    // Provide a DOM template for the element
   static get template() {
+    // Tag the returned template literal with the html helper function
+    // to convert it into an instance of HTMLTemplateElement
     return html`
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
@@ -15,12 +19,6 @@ class FooterComponent extends PolymerElement {
         footer{
             padding-top: 50px;
             background: #f6f6f6;
-        }
-        .app-cart-about{
-
-        }
-        .app-cart-links{
-
         }
         .app-cart-links ul li{
             margin-bottom: 5px;
@@ -51,20 +49,14 @@ class FooterComponent extends PolymerElement {
         .container{
             width: 80%!important;
         }
-        @media (max-width: 768px){
-        
-        }
-        @media (max-width: 600px){
-        
-        }
       </style>
         <footer>
             <div class="container">
                 <div class="row">
                     <div class="col m4 s12">
                         <div class="app-cart-about">
-                            <h5>UniqueHire Consulting LLP</h5>
-                            <p>the readable content of a page when looking at its layout 
+                            <h5 tabindex="0">UniqueHire Consulting LLP</h5>
+                            <p tabindex="0">the readable content of a page when looking at its layout 
                             the readable content of a page when looking at its layout
                             the readable content of a page when looking at its layout
                             the readable content of a page when looking at its layout </p>
@@ -85,9 +77,9 @@ class FooterComponent extends PolymerElement {
                     <div class="col m4 s12">
                         <div class="app-cart-location">
                             <address>
-                            <h5>Address</h5>
-                                <p>486, 1st Cross Rd, Aswath Nagar,</p>
-                                <p>Marathahalli, Bengaluru, Karnataka 560037</p>
+                            <h5 tabindex="0">Address</h5>
+                                <p tabindex="0">486, 1st Cross Rd, Aswath Nagar,</p>
+                                <p tabindex="0">Marathahalli, Bengaluru, Karnataka 560037</p>
                             </address>
                         </div>
                     </div>
@@ -95,11 +87,12 @@ class FooterComponent extends PolymerElement {
             </div>
             
             <div class="app-cart-copy-right">
-                <p>All rights reserved and maintained by <a href="uniquehire.in" class="foot-link-maintain" role="link">uniquehire</a></p>
+                <p tabindex="0">All rights reserved and maintained by <a href="uniquehire.in" class="foot-link-maintain" role="link">uniquehire</a></p>
             </div>
         </footer>
     `;
   }
 }
 
+//@customElement BestsellerProducts register to webbrowser
 window.customElements.define('footer-component', FooterComponent);
