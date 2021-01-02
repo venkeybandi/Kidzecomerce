a12b128b14d58738e7b161293b76f0120467a7d6
venkeybandi <70914716+venkeybandi@users.noreply.github.com>
2021-01-02 14:23:51 +0530

    Update bestseller-products.js

--- src/bestseller-products.js
+++ src/bestseller-products.js
@@ -1,91 +1,68 @@
 
+// Import the Polymer library and html helper function
+// Import the Polymer iron-ajax
+// Import the Polymer iron-image
+// Import the Base class AddToCart form addto-cart component
+// @customElement
+
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
-// import '@polymer/paper-tabs/paper-tabs.js';
+import { AddToCart } from './addtocart-operations.js';
 import '@polymer/iron-ajax/iron-ajax.js';
-// import '@polymer/paper-button/paper-button.js';
 import '@polymer/iron-image/iron-image.js';
-import '@polymer/iron-list/iron-list.js';
-import './shared-styles.js';
 import './shared-products-style.js';
 
+// Define the new element as a class
+// child-element renders its light DOM children inside this compnent
+class BestsellerProducts extends AddToCart {
 
-class BestsellerProducts extends PolymerElement {
+  // Provide a DOM template for the element
   static get template() {
+
+    // Tag the returned template literal with the html helper function
+    // to convert it into an instance of HTMLTemplateElement
     return html`
       <style include="shared-products-style">
-        
+        /* import custom styles and included shared-products-style  */
+        /* custom elements default to display: inline */
+
       </style>
 
-      <!-- iron-ajax element declaratively exposes network request functionality to Polymer's data-binding system
-       newarival-products json data.
-       handle-as: Specifies what data must be stored in the response
-        -->
+      <!-- iron-ajax element declaratively exposes network request functionality to Polymer's 
+      data-binding system newarival-products json data.
+      handle-as: Specifies what data must be stored in the response  -->
 
       <div class="products-sec">
         <iron-ajax 
           auto 
           url="./src/bestseller-data/bestseller-data.json" 
           handle-as="json" 
-          last-response="{{response}}"
-          on-response="handleResponse">
+          last-response="{{response}}">
         </iron-ajax>
         
-        <h1>Best Seller</h1>
-        <!--using iron-ajax response and do-repeat displaying json data and using one-way data binding-->
-        <iron-list items="[[response.results]]" as="item" id="itemlist" scroll-target="document" selected-item="{{selectedItem}}" selection-enabled grid>
-          <template>
-            <div class="products-container">
-              <div class="image">
-              <span class="labeltag">[[item.label]]</span>
-                <a href="">
-                  <img class="img" src='[[item.image]]'></img>
-                </a>
-                <h3>[[item.title]]</h3>
-                <h4><iron-icon class="small" src = "./src/images/currency-inr.svg"></iron-icon>[[item.price]]</h4>
-                <a href="Javascript:;" class="add-cart">Add cart</a>
-              </div>
+        <h1 tabindex="0">Best Seller</h1>
+        <div class="products-container"> <!-- products-container start-->
+        
+          <!--using iron-ajax response and do-repeat displaying json data and using one-way data binding-->
+          <template is="dom-repeat"  id="itemList" items="[[response.results]]" as="item" grid>
+            <div class="image" title="[[item.title]]" tabindex="0">
+              <span class="labeltag" title="[[item.label]]" tabindex="0">[[item.label]]</span>
+              <a href="[[rootPath]]productdetails" role="link">
+                <img class="img" id="img" src='[[item.image]]' alt='[[item.title]]' tabindex="0"></img>
+              </a>
+              <h3 id="title" class="title" title="[[item.title]]" role="title" tabindex="0">[[item.title]]</h3>
+              <h4 id="price" class="price" title="[[item.price]]" tabindex="0"><iron-icon class="small" src = "./src/images/currency-inr.svg"></iron-icon>[[item.price]]</h4>
+              <input type="submit" id="addtocart" on-click="addProduct" role="button" class="add-cart" value="Add Cart "></input>
             </div>
           </template>
-        </iron-list>
+          
+          <!-- Using  array-selector we are going to push the product details into arraylist -->
+          <array-selector id="selector" items="{{response.results}}" selected="{{selected}}" multi toggle></array-selector>
+        
+        </div> <!-- products-container end-->
       </div>
     `;
-  }
-
-
-  static get properties() { return { 
-    response: { type: Object },
-      _isRequest:{
-        type:Boolean,
-        value:true
-      }
-
     }
+
   }
-
-  static get observers(){ return ['_checkLastResponse(response)'] }
-
-  _checkLastResponse(r) {
-          // we have received a response from iron-ajax so allow for next call
-          if (r) {
-                  this._isRequest = true;
-          }
-  }
-
-  // As you provided above code, you call ajax manually. 
-  _ajax_call() {
-        if (this._isRequest) {
-
-        this._isRequest = false; 
-        setTimeout(()=> {
-                this.$.request.generateRequest();
-                
-        },4000)}   
-  }   
-
-     handleResponse(r) {
-       console.log(r)
-     }
-  }
-
-//registering to webbrowser using customelemet
+//@customElement BestsellerProducts register to webbrowser
 window.customElements.define('bestseller-products', BestsellerProducts);
